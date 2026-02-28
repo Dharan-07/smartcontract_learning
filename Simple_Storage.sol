26dd93ffd648904e44e7c76fb38d5bb33da7fbea
Dharan-07 <dharanishdharan2135@gmail.com>
2026-02-28 22:53:57 +0530

    The contract shows basic state management and struct usage in Solidity.

--- Simple_Storage.sol
+++ Simple_Storage.sol
@@ -3,14 +3,28 @@ pragma solidity ^0.8.18;
  
 contract SimpleStorage {
 
-    uint256 favNumber ;
+    uint256 myFavNumber ;
+
+    // uint256[] listOfFavNum; // array of 256 bit unsigned integers
+
+    struct Person {
+        uint256 favNumber;
+        string name;
+    } // create a new type Person using struct
+
+    Person[] public listOfPeople; // array of type Person
+
+    // Person public CR7 /* veriable of type Person */ = Person({favNumber : 7, name : "ronaldo"});
 
     function store (uint256 _favNumber) public {
-        favNumber = _favNumber;
-    } 
+        myFavNumber = _favNumber;
+    }  // this function use to update the state variable called favNumber
 
     function retrieve() public view returns (uint256) {
-        return favNumber;
+        return myFavNumber;
+    } // this function used to retrieve the modified value 
+
+    function addPerson(string memory _name, uint256 _favNumber) public {
+        listOfPeople.push( Person(_favNumber,_name));
     } 
-    
 }
