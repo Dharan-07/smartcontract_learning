9ea377a107f633eb57d6c17c4799ec6a849f59a3
Dharan-07 <dharanishdharan2135@gmail.com>
2026-02-28 19:59:27 +0530

    for storing my user value

---
+++ Simple_Storage.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18; 
+ 
+contract SimpleStorage {
+
+    uint256 favNumber ;
+
+    function store (uint256 _favNumber) public {
+        favNumber = _favNumber;
+    } 
+
+    function retrieve() public view returns (uint256) {
+        return favNumber;
+    } 
+    
+}
