cdeae5a15daff0ed10001c2f16e75c45665347ed
Dharan-07 <dharanishdharan2135@gmail.com>
2026-03-14 12:12:13 +0530

    .

--- practice.sol
+++ practice.sol
@@ -12,9 +12,23 @@ contract practice{
     person[] private contacts;
 
     mapping(string => uint256) public findNumByName ;
+    
 
     function addToContacts(uint256 _num, string memory _name, string memory _nickName) public {
         contacts.push(person(_num,_name,_nickName));
         findNumByName[_name] = _num;
+    }
+
+     function getcontacts(string memory _name) public view returns(uint256, string memory, string memory) {
+        for (uint256 i = 0; i < contacts.length; i++) {
+            if (keccak256(bytes(contacts[i].name)) == keccak256(bytes(_name))) {
+                return (contacts[i].num, contacts[i].name, contacts[i].nickName);
+            }
+        }
+        revert("Contact not found");
+    }
+
+    function getContact(uint256 _index)public view returns (uint256,string memory,string memory){
+        return (contacts[_index].num,contacts[_index].name,contacts[_index].nickName);
     }
 }
