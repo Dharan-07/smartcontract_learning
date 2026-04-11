1379ff9c06e38008bbbe6f084a65876a6829ddff
Dharan-07 <dharanishdharan2135@gmail.com>
2026-04-11 16:20:03 +0530

    .

--- contracts/FundMe_contracts/FundMe.sol
+++ contracts/FundMe_contracts/FundMe.sol
@@ -1,46 +1,25 @@
 //SPDX-License-Identifier:MIT
 pragma solidity ^0.8.18;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
 
 contract FundMe {
 
+    using PriceConverter for uint256;
+   
     uint256 public minimumUSD = 5e18;
     
     address [] public funders;
 
-    mapping(address funder => uint256 amount) public addressToAmountFunded;
+    mapping( address => uint256 ) public addressToAmountFunded;
 
     function fund() public payable{
-        // allow users to send $
-        // have a mini value $
-        // how we send ETH ?
-        require(getConvertionRate(msg.value) >= minimumUSD, "ur sending low value"/*error msg*/); // 1e18 = 1 ETH = 1000000000000000000 (wei) = 1*10^18 0r in code 1*10**18
+        require(msg.value.getConvertionRate() >= minimumUSD, "ur sending low value"/*error msg*/); // 1e18 = 1 ETH = 1000000000000000000 (wei) = 1*10^18 0r in code 1*10**18
         funders.push(msg.sender);
-        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value; // 
+        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value; 
     }
-
-   // function withdraw() public {}
-
-    function getPrice() public view returns (uint256) {
-        // address 0x694AA1769357215DE4FAC081bf1f309aDC325306 => this address used to get the price values of ETH/USD
-        // abi
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (, int256 answer,,, ) = priceFeed.latestRoundData();
-        // answer = price of ETH in terms of USD
-        return uint256(answer * 1e10);
-    } // from the github and documentation of chainlink
     
-    function getConvertionRate(uint256 ethAmount/*input*/) public view returns (uint256){
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
-        return ethAmountInUsd;
-    } 
+    function withdraw() public {}
 
-    function getVersion() public view returns(uint256) {
-       return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version(); 
-    } // In this function we are using the version() from the AggregatorV3Interface contract with the help of contract address interface and ABI
 }
 
-// what is revert ?
-// undo any action that have been done and send the remaining gas back
